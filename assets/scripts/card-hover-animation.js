d6a8da09b3f5e93b89c82e95eb61269da6ebae3c
Tarcila Cardoso Alves <tarcilacardosoalves@gmail.com>
2024-03-14 20:35:03 -0300

    aplicando transições nos cards

--- assets/scripts/card-hover-animation.js
+++ assets/scripts/card-hover-animation.js
@@ -1,19 +1,25 @@
-function handleMouseEnter(){
-    console.log('handle mouse enter called', this); /* é para verificar se a função está sendo chamada */
+function handleMouseEnter(){    
+    this.classList.add('s-card--hovered');   
+    document.body.id = `${this.id}-hovered`;
+}
+/*Sempre que o mouse saí desse elemento, precisamos que ele remova o classe --hovered */
+
+function handleMouseLeave(){
+    this.classList.remove('s-card--hovered');
+    document.body.id = ` `;
+
 }
 
 
 
 function addEventListenersToCards(){
     const cardElements = document.getElementsByClassName('s-card');
-    console.log(cardElements);
-    /* Queremos que quando o usuário coloque o mouse sobre o card ele adicione uma nova classe css 
-    sobre esse elemento dinamicamente. Sabemos que um array e tem varios elementos, não vamos 
-    conseguir colocar somente com uma função, uma classe dentro de uma array, então devemos fazer um looping
-    usando o 'For' */
+    
+
     for(let index = 0; index < cardElements.length; index++){
-        const card = card = cardElements[index];
+        const card = cardElements[index];
         card.addEventListener('mouseenter', handleMouseEnter);
+        card.addEventListener('mouseleave', handleMouseLeave);
     }
 
 }
