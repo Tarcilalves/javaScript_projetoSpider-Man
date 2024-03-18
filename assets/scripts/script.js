55f7701e8eb96a21b76439fad9f6b8cbe7c890f3
Tarcila Cardoso Alves <tarcilacardosoalves@gmail.com>
2024-03-18 20:49:29 -0300

    carrossel 3D

---
+++ assets/scripts/script.js
@@ -0,0 +1,27 @@
+function handleMouseEnter(){    
+    this.classList.add('s-card--hovered');   
+    document.body.id = `${this.id}-hovered`;
+}
+/*Sempre que o mouse saí desse elemento, precisamos que ele remova o classe --hovered */
+
+function handleMouseLeave(){
+    this.classList.remove('s-card--hovered');
+    document.body.id = ` `;
+
+}
+
+
+
+function addEventListenersToCards(){
+    const cardElements = document.getElementsByClassName('s-card');
+    
+
+    for(let index = 0; index < cardElements.length; index++){
+        const card = cardElements[index];
+        card.addEventListener('mouseenter', handleMouseEnter);
+        card.addEventListener('mouseleave', handleMouseLeave);
+    }
+
+}
+
+document.addEventListener("DOMContentLoaded", addEventListenersToCards, false) 
